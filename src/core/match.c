89b4c4f3c0ac22bd4b365d998ba7d6a4ae91cf28
Omega-090 <carlmathewbartolo@gmail.com>
2026-05-11 22:14:47 +0800

    Update match.c

--- src/core/match.c
+++ src/core/match.c
@@ -11,3 +11,20 @@ int main() {
 
 }
 
+void match_add_score(MatchState *m, int player){
+
+  if (player == 1){
+    m->p1
+}
+
+void match_add_fault(MatchState *m, int player){
+
+}
+
+void match_add_ace(MatchState *m, int player){
+
+}
+
+void match_undo(MatchState *m){
+
+}
