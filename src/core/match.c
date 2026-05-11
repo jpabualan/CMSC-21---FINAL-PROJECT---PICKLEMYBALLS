bf23ad5ffe32f3e6caa91b6e8f2d4a4463789245
Omega-090 <carlmathewbartolo@gmail.com>
2026-05-12 00:38:18 +0800

    Update match.c

--- src/core/match.c
+++ src/core/match.c
@@ -6,14 +6,23 @@ void match_init(MatchState *m, const char *name1, const char *name2){
     memset(m, 0, sizeof(MatchState)); // Sets all of the bytes in a block of memory to the same value(0)
 
     strncpy(m->p1.name, name1, MAX_NAME_LEN); //init player 1
-    m->p1.is_serving = 1; //player 1 is serving
-
     strncpy(m->p2.name, name2, MAX_NAME_LEN); //init player 2
 
+    m->p1.score = 0;
+    m->p2.score = 0;
+
+    m->p1.faults = 0;
+    m->p2.faults = 0;
+
+    m->p1.aces = 0;
+    m->p2.aces = 0;
+
+    m->game_over = 0;
+    m->winner = 0;
     m->current_set = 0;
+
     m->timer_running = 0;
     m->ball_in = 1;
-
 }
 
 void match_add_score(MatchState *m, int player){
