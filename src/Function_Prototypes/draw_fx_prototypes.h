16a6728afdd4bec5fcb28181253be2bf8c851290
Rafael <rafael.iii.victoriano1@gmail.com>
2026-05-16 00:52:37 +0800

    added drawMenuButton

--- src/Function_Prototypes/draw_fx_prototypes.h
+++ src/Function_Prototypes/draw_fx_prototypes.h
@@ -7,5 +7,6 @@ void DrawMainMenu(Appstate *state);
 void DrawGameScreen(Appstate *state);
 void DrawHistoryScreen(Appstate *state);
 void DrawCourt(Appstate *state, Camera3D cam);
+bool drawMenuButton(Rectangle bounds, const char *text, Color baseColor, Vector2 mouse, bool enabled);
 
 #endif
